a886ee63c869629b50ba022f19e5eadd12edceff
chenjiancheng <1482409829@qq.com>
2022-11-18 23:02:33 +0800

    feat: 引入html2canvas

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -4,11 +4,14 @@ import useAction from "../../hook/useAction";
 import { ActionTypes } from "../../state/constants/actionTypes";
 import { State } from "../../state/reducer";
 import styles from "./index.less";
+import html2canvas from "html2canvas";
 
 const Toolbar = function () {
   const editorConfig = useSelector(
     (state: State) => state.editor.canvasStyleData
   );
+
+  const { editor } = useSelector((state: State) => state.editor);
   const dispatch = useDispatch();
   const { setCurComponent, setComponetData, undo, redo } = useAction();
 
@@ -28,20 +31,19 @@ const Toolbar = function () {
     setComponetData([]);
   };
 
+  const handleScreenshot = () => {
+    html2canvas(editor, { useCORS: true }).then(function (canvas) {
+      document.body.appendChild(canvas);
+    });
+  };
+
   return (
     <Space className={styles.toolbar}>
       {/* <Button>JSON</Button> */}
       <Button onClick={undo}>撤销</Button>
       <Button onClick={redo}>重做</Button>
-      {/* <Button>插入图片</Button>
-      <Button>预览</Button>
-      <Button>保存</Button> */}
       <Button onClick={clearCanvas}>清空画布</Button>
-      {/* <Button>组合</Button>
-      <Button>拆分</Button>
-      <Button>锁定</Button>
-      <Button>解锁</Button>
-      <Button>截图</Button> */}
+      <Button onClick={handleScreenshot}>截图</Button>
       <div className={styles["canvas-config"]}>
         <span>画布大小</span>
         <InputNumber
